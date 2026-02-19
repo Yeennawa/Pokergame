66f4e9476b55e0d876c21f28251aeba3f47ced77
Yeennawa <nwminthrchuncitr78@gmail.com>
2026-02-20 01:06:02 +0700

    qqqqqq

--- src/baccaratgame/Menu.java
+++ src/baccaratgame/Menu.java
@@ -4,15 +4,15 @@ import java.awt.*;
 
 public class Menu extends JPanel {
     GameFrame frame;
+
     public Menu(GameFrame frame) {
         this.frame = frame;
         this.setLayout(null);
-        this.setPreferredSize(new Dimension(1366, 768));
+        this.setPreferredSize(new Dimension(800, 800));
 
         JLabel bg = background();
-        bg.setBounds(0, 0, 1366, 768);
+        bg.setBounds(0, 0, 800, 800);
         this.add(bg);
-
     }
 
     private JButton startButton() {
@@ -20,7 +20,11 @@ public class Menu extends JPanel {
         startButton.addActionListener(e -> {
             frame.startGame();
         });
-        startButton.setPreferredSize(new Dimension(200, 50));
+
+        startButton.setBackground(new Color(184, 134, 11));
+        startButton.setForeground(Color.WHITE);
+        startButton.setFocusPainted(false);
+        startButton.setFont(new Font("Arial", Font.BOLD, 18));
 
         return startButton;
     }
@@ -30,7 +34,11 @@ public class Menu extends JPanel {
         exitButton.addActionListener(e -> {
             System.exit(0);
         });
-        exitButton.setPreferredSize(new Dimension(200, 50));
+
+        exitButton.setBackground(new Color(60, 0, 0));
+        exitButton.setForeground(new Color(255, 215, 0));
+        exitButton.setFocusPainted(false);
+        exitButton.setFont(new Font("Arial", Font.BOLD, 18));
 
         return exitButton;
     }
@@ -39,25 +47,30 @@ public class Menu extends JPanel {
         java.net.URL imgURL = getClass().getResource("/Baccaratimgame/Game front page.jpg");
         ImageIcon background;
 
+        int w = 800;
+        int h = 800;
+
         if (imgURL != null) {
-            background = new ImageIcon(imgURL);
+            ImageIcon originalIcon = new ImageIcon(imgURL);
+            Image scaledImage = originalIcon.getImage().getScaledInstance(w, h, Image.SCALE_SMOOTH);
+            background = new ImageIcon(scaledImage);
         } else {
-            System.err.println("หาภาพพื้นหลังไม่เจอที่: /images/backfree.jpg");
-            background = new ImageIcon(); // สร้าง Object ว่างเพื่อไม่ให้โปรแกรม Crash
+            background = new ImageIcon();
         }
 
         JLabel label = new JLabel(background);
         label.setLayout(null);
 
-
+        int bx = (w / 2) - (220 / 2);
 
         JButton start = startButton();
-        start.setBounds(150, 350, 175, 40);
+        start.setBounds(bx, 620, 220, 50);
         label.add(start);
 
         JButton exit = exitButton();
-        exit.setBounds(150, 400, 175, 40);
+        exit.setBounds(bx, 680, 220, 50);
         label.add(exit);
 
         return label;
-    }}
+    }
+}
