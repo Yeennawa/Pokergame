c47e8fa3acf3afb4175e9397091668afcaf4982d
Yeennawa <nwminthrchuncitr78@gmail.com>
2026-02-20 01:01:53 +0700

    qqqqqq

--- src/baccaratgame/GameFrame.java
+++ src/baccaratgame/GameFrame.java
@@ -1,31 +1,37 @@
 package baccaratgame;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameFrame extends JFrame {
     public GameFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Baccarat Game");
         this.setResizable(false);
-        this.add(new Menu(this));
-        this.pack();
-        this.setLocationRelativeTo(null);
+        this.setLayout(new GridBagLayout());
+
+        showMenu();
         this.setVisible(true);
     }
+
     public void showMenu() {
         getContentPane().removeAll();
+        this.setLayout(new GridBagLayout());
         Menu menu = new Menu(this);
-        this.add(menu);
+        this.add(menu, new GridBagConstraints());
         this.pack();
+        this.setLocationRelativeTo(null);
         revalidate();
         repaint();
     }
 
     public void startGame(){
         this.getContentPane().removeAll();
+        this.setLayout(new GridBagLayout());
         GamePanel panel = new GamePanel(this);
-        this.add(panel);
+        this.add(panel, new GridBagConstraints());
         this.pack();
+        this.setLocationRelativeTo(null);
         this.revalidate();
         this.repaint();
         panel.requestFocus();
